63d4113fbcdb4283486c2d96924c2fce5b228f00
Pablo Pita <Pablo.Pita@gmail.com>
2012-12-04 18:31:17 +0000

    shorter package name

---
+++ sim/src/main/java/org/osk/factories/LogFactory.java
@@ -0,0 +1,16 @@
+package org.osk.factories;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogFactory {
+
+	@Produces 
+	Logger createLogger( InjectionPoint injectionPoint ) { 
+		String className = injectionPoint.getMember().getDeclaringClass().getName();
+		return LoggerFactory.getLogger( className ); 
+	}
+}
